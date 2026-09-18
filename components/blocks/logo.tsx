81ac8eb0760d7f3eb310bc77c2cecd89920ca741
Bastian Cruvellier <b.cruvellier@MacBook-Pro-de-Bastian.local>
2026-09-18 14:35:02 +0200

    fix(frontend): logo size error

--- components/blocks/logo.tsx
+++ components/blocks/logo.tsx
@@ -12,6 +12,7 @@ export function Logo({ showName = true }: LogoProps) {
         alt="Odin"
         width={32}
         height={32}
+        className="h-8 w-8"
       />
       {showName && (
         <span className="ml-2 font-logo text-lg font-bold tracking-tight uppercase">Odin</span>
